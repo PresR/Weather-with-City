ccd5e2016dba61e0e118ef037c4a9e5d870ec1e2
PresR <152711990+PresR@users.noreply.github.com>
2024-03-10 15:19:20 +0000

    changed index.js to input latitude and longitude

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ app.get("/", function(req, res) {
 //invoked after hitting go in the html form
 app.post("/", function(req, res) {
 
-    // takes in the city from the html form, display in console
-    var city = String(req.body.cityInput);
-    console.log(req.body.cityInput);
+    // takes in the latitude and longitude from the html form
+    const latitude = req.body.latitudeInput;
+    const longitude = req.body.longitudeInput;
 
     // build up the URL for the JSON query, API Key is secret and needs to be obtained by signup
     const units = "imperial";
     const apiKey = "d80845cc828106ba8005e5e86e8a8792";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + units + "&APPID=" + apiKey;
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=" + units + "&APPID=" + apiKey;
 
     // this gets the data from Open Weather API
     https.get(url, function(response){
@@ -31,17 +31,16 @@ app.post("/", function(req, res) {
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
-          // added new individual items
+            // added new individual items
             const humidity = weatherData.main.humidity;
             const windSpeed = weatherData.wind.speed;
-          
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
 
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The Temperature at Latitude " + latitude + ", Longitude " + longitude + " is " + temp + " Degrees Fahrenheit<h2>");
             // displays humidity and wind speed
             res.write("<p>Humidity: " + humidity + "%</p>");
             res.write("<p>Wind Speed: " + windSpeed + " mph</p>");
